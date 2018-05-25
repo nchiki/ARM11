39dfdf7c9ff09916f9410ce5ff198994b9308bb0
blanca <blancatjm@gmail.com>
2018-05-25 17:45:08 +0100

    pipeline started in main, some problems fetching the instruction

--- src/emulate.c
+++ src/emulate.c
@@ -1,12 +1,17 @@
 #include <stdlib.h>
+#include <stdbool.h>
 #include "memoryImplementation.h"
 #include "binaryloader.c"
 
   int main(int argc, char **argv) {
-    uint32_t *registerArray = (uint32_t *) calloc(17, sizeof(uint32_t));
+
     uint32_t *memArray = (uint32_t *) calloc(16384, sizeof(uint32_t));
-    uint32_t *start = *loadFile(argv[1], )
-    while(instruction != 0x0) {
+    uint32_t *start = *loadFile(argv[1], 0);
+    uint32_t *registerArray = (uint32_t *) calloc(17, sizeof(uint32_t));
+    bool finalise = false;
+
+    while (!finalise) {
+      uint32_t instruction = *registerArray[PC];
       int cond = instruction >> 28;
       switch (cond) {
 
@@ -19,4 +24,21 @@
       free(*registerArray);
       return 0;
     }
+
+
   }
+
+  uint32_t fetch(uint32_t registerArray) {
+    uint32_t instr = (*registerArray)[15]; //taking the address stored in the PC
+    *registerArray[PC]++; // incrementing PC
+    return instr;
+  }
+
+  bool decode(uint32_t instruction) {
+
+    return true;
+}
+
+  void execute(uint32_t instruction) {
+
+}
