6fc8983935f7c47784f12e27f0231c7c1f2fd532
nb1317 <niranjan.bhat17@imperial.ac.uk>
2018-05-25 22:19:08 +0100

    wrote 2 big comments detailing the usage of a global variable for machine, and about whether or not to make instructions a struct

--- src/emulate.c
+++ src/emulate.c
@@ -7,7 +7,12 @@
 
 
   int main(int argc, char **argv) {
-
+    /* explanation:
+     * would it not be easier to just use the global variable machine from memImplementation.h rather than create a new
+     * one and have to mess around with pointers and what not?
+     * at the start of this function, main, just initialise everything to zero. doesn't mean the struct variable cant
+     * exist before this, does it?
+    */
 
     for ( int i = 0 ; i < 17; ++i ) {
       machine.registers[i] = 0;
@@ -30,9 +35,28 @@
 
     // read from binary file into memory array
     // i wonder if i could do this : loadFile(givenFile, memArray) -> technically it should be fine because they both point to memAlloc[0]?
-
-    
     loadFile(givenFile,machine.memoryAlloc);
+
+
+    /* for the main while loop of emulate:
+     * is it better to create a struct for instructions, and then extract one uint32 from memory and use it, or is it
+     * better to not do any such thing and just process the "instructions" in a 3-staggered while loop?
+     * if instructions is it's own struct:
+     *      then it can have data members instruction_type, cond and a bunch of others to store the effects and particulars
+     *      of that particular instruction, but this may lead to problems with dereferencing later
+     * if instructions is NOT it's own struct:
+     *      then as soon as we extract from memory, there needs to be a helper function which extracts all the necessary
+     *      information like instructionType, cond, opcode and other particulars of the instruction. could stick these
+     *      functions into a .h and .c file of their own, or could have them inside the while loop which would make it
+     *      quite messy
+     *      ^^^ oh i was talking about decode here and in the point above.
+     *
+     * essentially, it comes down to having a helper functions to perform decode, or having decode within the struct      
+     * design decision here
+     */
+
+
+
 
 
     bool finalise = false;
