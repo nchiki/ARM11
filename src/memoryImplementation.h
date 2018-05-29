07a527fae30a6c09b63e5c4d574de07bad3f9ae2
nb1317 <nbhat1998@gmail.com>
2018-05-29 16:55:59 +0100

    with gio, just started changing the structures for memory implementations. INCOMPLETE, DO NOT PULL THIS

--- src/memoryImplementation.h
+++ src/memoryImplementation.h
@@ -10,13 +10,26 @@
 #define CPSR 16
 
 //holds the state of the program
+
+
+typedef struct{
+    uint32_t memoryAlloc[16384];
+} MEMORY;
+
 typedef struct{
     uint32_t registers[17];
-    uint32_t memoryAlloc[16384];
     bool instructionFetched;
     uint32_t fetchedInstruction;
     instruction *decodedInstruction;
-} machine_type;
+} CPU;
 
+
+typedef struct {
+    memory mem;
+    cpu c ;
+} MACHINE;
 
 #endif //ARM11_28_MEMORYIMPLEMENTATION_H
+
+
+memory mach
