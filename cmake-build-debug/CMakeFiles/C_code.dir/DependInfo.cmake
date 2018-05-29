7579ddd9aec68b5decd396073968d9fce07b89c9
nc1517 <nc1517@ic.ac.uk>
2018-05-29 12:34:14 +0100

    branch execution added to switch

---
+++ cmake-build-debug/CMakeFiles/C_code.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/nahida/Documents/C/C_code/src/assemble.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/assemble.c.o"
+  "/home/nahida/Documents/C/C_code/src/binaryloader.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/binaryloader.c.o"
+  "/home/nahida/Documents/C/C_code/src/decode.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/decode.c.o"
+  "/home/nahida/Documents/C/C_code/src/emulate.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/emulate.c.o"
+  "/home/nahida/Documents/C/C_code/src/execute.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/execute.c.o"
+  "/home/nahida/Documents/C/C_code/src/instructions/DataProcDec.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/instructions/DataProcDec.c.o"
+  "/home/nahida/Documents/C/C_code/src/instructions/branch.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/instructions/branch.c.o"
+  "/home/nahida/Documents/C/C_code/src/instructions/multiply.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/instructions/multiply.c.o"
+  "/home/nahida/Documents/C/C_code/src/instructions/singleDT.c" "/home/nahida/Documents/C/C_code/cmake-build-debug/CMakeFiles/C_code.dir/src/instructions/singleDT.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/C_code.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_code.dir/src/assemble.c.o"
+  "CMakeFiles/C_code.dir/src/emulate.c.o"
+  "CMakeFiles/C_code.dir/src/binaryloader.c.o"
+  "CMakeFiles/C_code.dir/src/instructions/singleDT.c.o"
+  "CMakeFiles/C_code.dir/src/execute.c.o"
+  "CMakeFiles/C_code.dir/src/decode.c.o"
+  "CMakeFiles/C_code.dir/src/instructions/multiply.c.o"
+  "CMakeFiles/C_code.dir/src/instructions/branch.c.o"
+  "CMakeFiles/C_code.dir/src/instructions/DataProcDec.c.o"
+  "C_code.pdb"
+  "C_code"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
