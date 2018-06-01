8ba70490ea9eb1451d975787e6b5d71ea7493dc4
nb1317 <nbhat1998@gmail.com>
2018-06-01 17:16:44 +0100

    bugfix #4

---
+++ src/instructions/multiply.h
@@ -0,0 +1,16 @@
+//
+// Created by nb1317 on 01/06/18.
+//
+
+#ifndef ARM11_28_MULTIPLY_H
+#define ARM11_28_MULTIPLY_H
+#include <stdlib.h>
+#include <stdio.h>
+#include "../memoryImplementation.h"
+#include "../usefulTools.h"
+#include "../instruction_basic.h"
+#include "../usefulFuncs.h"
+
+void multiply(uint32_t , MACHINE* );
+
+#endif //ARM11_28_MULTIPLY_H
