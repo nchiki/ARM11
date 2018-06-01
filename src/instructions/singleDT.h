43f65c2c60f99e21f599721999c01d8e83b7ebfd
nb1317 <nbhat1998@gmail.com>
2018-06-01 17:19:14 +0100

    bugfix#5

---
+++ src/instructions/singleDT.h
@@ -0,0 +1,16 @@
+//
+// Created by nb1317 on 01/06/18.
+//
+
+#ifndef ARM11_28_SINGLEDT_H
+#define ARM11_28_SINGLEDT_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include "../memoryImplementation.h"
+#include "../instruction_basic.h"
+#include "../usefulTools.h"
+#include "../usefulFuncs.h"
+void SDTdecode(uint32_t, MACHINE* );
+#endif //ARM11_28_SINGLEDT_H
