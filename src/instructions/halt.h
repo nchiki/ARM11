450980925d3d4896ed7db0d515b6e7199780e7a7
nb1317 <nbhat1998@gmail.com>
2018-06-01 17:15:09 +0100

    bugfix #3

---
+++ src/instructions/halt.h
@@ -0,0 +1,15 @@
+//
+// Created by nb1317 on 01/06/18.
+//
+
+#ifndef ARM11_28_HALT_H
+#define ARM11_28_HALT_H
+
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdint.h>
+#include "../memoryImplementation.h"
+
+void halt (MACHINE* );
+#endif //ARM11_28_HALT_H
