8cd3a3aebeb6cd823c0ed7f9ad012b52a64baafa
nb1317 <nbhat1998@gmail.com>
2018-06-01 16:05:30 +0100

    BIG UPDATE- implemented ADT and ADT functions

--- src/assemblerImplementation.h
+++ src/assemblerImplementation.h
@@ -8,14 +8,22 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct {
+struct symbol {
 
     char *label;
     uint32_t address;
     struct symbol *next;
-} symbol;
 
-symbol *symbolTableHead;
+    void clear() {
+        free(this->label);
+        free(this);
+    }
+};
+
+// purposely did not use typedef because typedef breaks a couple of things in the implementation of addLabel in assemblerImplementation.c
+// DO NOT CHANGE TO TYPEDEF STRUCT {..}SYMBOL;
+
+struct symbol *symbolTableHead;
 
 void initSymbolTable(void);
 
@@ -23,6 +31,6 @@ void addLabel (char*, uint32_t) ;
 
 uint32_t getAddress (char*);
 
-void clearSymbolTable(void);
+void clearSymbolTable();
 
 #endif //ARM11_28_ASSEMBLERIMPLEMENTATION_H
