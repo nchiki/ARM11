c3bdaf867c9b7259ead8879a8c3afbc2f77b6a70
blanca <blancatjm@gmail.com>
2018-06-02 20:12:39 +0200

    starting to implement main program assembler

--- src/assemble.c
+++ src/assemble.c
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <assert.h>
+#include "memoryImplementation.h"
+#include "emulate_utils/instruction_basic.h"
+#include "assemble_utils/assemblerImplementation.h"
 
 int main(int argc, char **argv) {
 
@@ -9,6 +12,14 @@ int main(int argc, char **argv) {
 
     char *destFile = argv[2];
 
-
+    instructions thisInstruction = NULL;
+    symbolTableHead->label = "A:";
+    symbolTableHead->address = 0;
+    symbolTableHead->next =
+    //executes until it reaches the end of the program
+    while (thisInstruction.type != Halt) {
+        //first pass
+        symbolTableHead
+    }
 
 }
