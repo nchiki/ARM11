d90984c7db70a33840a2aca9db6c40c332784949
nb1317 <nbhat1998@gmail.com>
2018-06-03 15:14:18 +0100

    started fixing getBitRange

--- src/usefulFuncs.c
+++ src/usefulFuncs.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "usefulFuncs.h"
+#include "emulate_utils/instruction_basic.h"
 
 //extracts any number of bits from any starting index
-uint32_t getBitRange(uint32_t num, int start, int length){
+uint32_t getBitRange(instructions num, int start, int length){
     uint32_t res = 0;
     for(int i = start; i < start + length; i++){
         res += (1 << i) & num;
